2cdb373174b9b14cfd10ca5be72a7d17d23925ed
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-18 18:20:32 -0700

    Cursor is clickable

--- react-client/src/components/Cursor.jsx
+++ react-client/src/components/Cursor.jsx
@@ -6,11 +6,7 @@ import {Entity, Scene, Options} from 'aframe-react';
 import React from 'react';
 
 const Cursor = props => (
-  <a-entity position="0 1.8 4">
-    <a-entity camera look-controls mouse-cursor>
-      <a-cursor fuse="true" color="yellow"></a-cursor>
-    </a-entity>
-  </a-entity>
+  <a-entity camera look-controls mouse-cursor/>
 );
 
 export default Cursor;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
       items: []
     }
   }
-  
+
 
   componentDidMount() {
     //
@@ -23,7 +23,7 @@ class App extends React.Component {
   render () { 
     return (
       <Scene>
-        <a-box position="0 3.5 -2" rotation="30 30 0" color="skyblue"
+        <a-box position="0 1.5 -4" rotation="30 30 0" color="skyblue"
             event-set__enter="_event: mouseenter; material.color: yellowgreen; scale: 3 1 1"
             event-set__leave="_event: mouseleave; material.color: skyblue; scale: 1 1 1">
           <a-animation attribute="rotation" begin="click" dur="500" fill="backwards" to="30 30 360"></a-animation>
@@ -33,8 +33,8 @@ class App extends React.Component {
               autoPlay loop></video> 
           <img id="story" src="https://s3-us-west-1.amazonaws.com/vrstories/1500134536083" crossOrigin="anonymous"></img>
         </a-assets> 
-        <a-videosphere src="#video" rotation="0 -90 0"></a-videosphere> 
-        {/* <a-sky src="#story"></a-sky> */}
+        <a-videosphere src="#video" rotation="0 -90 0"></a-videosphere>     
+        {/* <a-sky src="#story"></a-sky>     */}
         <Cursor />
       </Scene>
     )
